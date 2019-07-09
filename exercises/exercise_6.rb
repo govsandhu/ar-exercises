f60889ee94e7c5d95eeb99645b877d7149b27a88
govsandhu <gc.sandhu@gmail.com>
2019-07-09 21:02:56 +0000

    Solution 7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -5,7 +7,18 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 60)
+@store1.employees.create(first_name: 'Lindsay', last_name: 'Andy', hourly_rate: 60)
+@store1.employees.create(first_name: 'Borsa', last_name: 'Travis', hourly_rate: 60)
+
+@store2.employees.create(first_name: 'Sandhu', last_name: 'Govind', hourly_rate: 60)
+@store2.employees.create(first_name: 'Wing', last_name: 'Steven', hourly_rate: 60)
+@store2.employees.create(first_name: 'Sinnot', last_name: 'Ryan', hourly_rate: 60)
+
+
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -6,7 +8,20 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+
+puts 'What is the name of the store?'
+print '>'
+
+@user_input = gets.chomp
+
+new_store = Store.create(name: @user_input)
+
+@errors = new_store.errors.messages if new_store.valid? == false
+
+@errors.each do |error, message|
+  puts "#{error}: #{message[0]}"
+end
